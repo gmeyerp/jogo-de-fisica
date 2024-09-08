ca30fe7f73c7bb04f6b37d7d980a0c08663a19b0
Gabriel Meyer <gmeyerp@gmail.com>
2024-09-08 17:56:05 -0300

    Ajustes movimento Player
    FallTrigger funcionando
    Limite do pulo do jogador
    Player invencível quando recebe dano de contato ou queda
    Está fora do escopo desse branch então vou encerrar ele

--- Jogo de Fisica/Assets/Scripts/PlayerMovement.cs
+++ Jogo de Fisica/Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public static PlayerMovement instance;
+    [Header("Movement")]
     [SerializeField] Rigidbody rb;
     [SerializeField] float speed;
     [SerializeField] float jumpForce;
-    Vector3 lastSafePosition;
     Vector3 movement;
+    bool isGrounded;
+
+    [Header("Damage")]
+    [SerializeField] float invincibilityTime = 1f;
+    bool isInvincible;
     #region Actions
     public InputActionAsset actions;
     InputActionMap player;
@@ -44,8 +50,12 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        GameManagement.instance.SetSafePosition(transform.position);
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange); // Se debuffs afetarem o peso isso deve ser alterado
+        if (isGrounded)
+        {
+            //GameManagement.instance.SetSafePosition(transform.position); nao parece necessario com OnCollisionExit
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange); // Se debuffs afetarem o peso isso deve ser alterado
+        }
+        isGrounded = false;
     }
 
     void OnEnable()
@@ -59,6 +69,34 @@ public class PlayerMovement : MonoBehaviour
 
     public void TakeDamage()
     {
-        GameManagement.instance.ReduceHealth();
+        if (!isInvincible) 
+        {
+            StartCoroutine(InvincibilityOn(invincibilityTime));
+            GameManagement.instance.ReduceHealth();
+        }
     }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.gameObject.layer == 7)
+        {
+            GameManagement.instance.SetSafePosition(collision.collider.gameObject.transform.position - Vector3.forward);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.layer == 7)
+        {
+            isGrounded = true;
+        }
+    }
+
+    IEnumerator InvincibilityOn(float time)
+    {
+        isInvincible = true;
+        yield return new WaitForSeconds(time);
+        isInvincible = false;
+    }
+
 }
